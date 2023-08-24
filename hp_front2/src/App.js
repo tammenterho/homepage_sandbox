bbadc2b32ba9fdd4f86a96bb51788bd099771634
tammenterho <terho.mikko@gmail.com>
2023-08-24 10:46:03 +0300

    skeleton for ccs cheeky social sorbet

--- hp_front2/src/App.js
+++ hp_front2/src/App.js
@@ -20,6 +20,50 @@ function App() {
           and innovation to your brand.
         </p>
       </div>
+      <div>
+        <h3>Client Case 1: WhimsicalWares E-Commerce</h3>
+        <p>
+          For WhimsicalWares, an online store selling unique handcrafted goods,
+          we concocted a social media strategy that combined quirky product
+          showcases with playful storytelling. By infusing humor and
+          relatability into their posts, we boosted engagement and transformed
+          their followers into avid customers, turning each scroll into a
+          delightful journey.
+        </p>
+      </div>
+      <div>
+        <h3>Client Case 2: MemeMasters Gaming</h3>
+        <p>
+          MemeMasters Gaming wanted to level up their social media game. We
+          crafted a campaign that fused gaming humor with viral trends,
+          skyrocketing their follower count and building an enthusiastic
+          community. Our cheeky posts and interactive challenges turned
+          MemeMasters into more than just a game – it became an experience worth
+          sharing.
+        </p>
+      </div>
+      <div>
+        <h3>Client Case 3: FitFunnies Fitness</h3>
+        <p>
+          FitFunnies Fitness sought a way to stand out in the saturated fitness
+          market. We devised a content strategy that fused motivational workout
+          tips with amusing gym anecdotes. Through relatable content, witty
+          captions, and even the occasional workout blooper, we helped
+          FitFunnies Fitness become the go-to destination for fitness
+          enthusiasts seeking a laugh.
+        </p>
+      </div>
+      <div>
+        <h3>Client Case 4: BuzzBites Café</h3>
+        <p>
+          To give BuzzBites Café a distinctive online presence, we whipped up a
+          campaign that combined mouthwatering food visuals with humor-filled
+          captions. From playful coffee puns to witty descriptions of their
+          delectable treats, our strategy transformed BuzzBites into an
+          Instagram sensation that not only satisfied cravings but also tickled
+          taste buds.
+        </p>
+      </div>
     </div>
   );
 }
