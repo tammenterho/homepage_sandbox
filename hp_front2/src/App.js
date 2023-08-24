e4e42619cb6c5147abcf64487ef0aced7a20ae2e
tammenterho <terho.mikko@gmail.com>
2023-08-24 13:23:15 +0300

    crafting image

--- hp_front2/src/App.js
+++ hp_front2/src/App.js
@@ -10,17 +10,17 @@ function App() {
             alt="cheeky sorbet logo"
           />
           <h1 className="otsikko">Cheeky Social Sorbet</h1>
+          <div className="nav">
+            <p>works</p>
+            <p>contact</p>
+            <p>about us</p>
+          </div>
           <p className="slogan">
             At Cheeky Social Sorbet, we're not your average digital marketing
             agency – we're the zing in your online presence! Just like a
             refreshing sorbet cleanses your palate, we bring a burst of
             creativity and innovation to your brand.
           </p>
-          <div className="nav">
-            <p>works</p>
-            <p>contact</p>
-            <p>about us</p>
-          </div>
         </div>
       </div>
       <div className="works">
