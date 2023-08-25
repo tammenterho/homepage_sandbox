a39f2036ca31c4bbc2371f8b31956ec6586c8e3c
tammenterho <terho.mikko@gmail.com>
2023-08-25 18:11:53 +0300

    more html

--- hp_front2/src/App.js
+++ hp_front2/src/App.js
@@ -34,28 +34,34 @@ function App() {
       <div className="aboutus">
         <h2>About us</h2>
       </div>
-      <div>
-        <p>Ilta Hämähäkkänen</p>
-        <p>Creative Visionary</p>
-        <p>
-          Ilta is the visionary guiding the creative direction at Cheeky Social
-          Sorbet. Her unique perspective and innovative solutions make her an
-          inspiration to the entire team.
-        </p>
-        <p>Mikael Nautavaara</p>
-        <p>Visuaalinen Virtuoosi</p>
-        <p>
-          Mikael crafts visual masterpieces that elevate the brands of Cheeky
-          Social Sorbet's clients to new heights. His ability to combine colors,
-          shapes, and narratives is unparalleled.
-        </p>
-        <p>Sylvi-Petra Lehti-Kokkeli</p>
-        <p>Social Alchemist</p>
-        <p>
-          Sylvi is a social media magician conjuring enchanting campaigns at
-          Cheeky Social Sorbet. Her deep understanding of algorithms and knack
-          for predicting trends make her a true social media expert.
-        </p>
+      <div className="aboutus-cont">
+        <div>
+          <h3>Ilta Hämähäkkänen</h3>
+          <h4>Creative Visionary</h4>
+          <p>
+            Ilta is the visionary guiding the creative direction at Cheeky
+            Social Sorbet. Her unique perspective and innovative solutions make
+            her an inspiration to the entire team.
+          </p>
+        </div>
+        <div className="mikael">
+          <h3>Mikael Nautavaara</h3>
+          <h4>Visual Artisan</h4>
+          <p>
+            Mikael crafts visual masterpieces that elevate the brands of Cheeky
+            Social Sorbet's clients to new heights. His ability to combine
+            colors, shapes, and narratives is unparalleled.
+          </p>
+        </div>
+        <div>
+          <h3>Sylvi-Petra Lehti-Kokkeli</h3>
+          <h4>Social Alchemist</h4>
+          <p>
+            Sylvi is a social media magician conjuring enchanting campaigns at
+            Cheeky Social Sorbet. Her deep understanding of algorithms and knack
+            for predicting trends make her a true social media expert.
+          </p>
+        </div>
       </div>
       <div className="contact">
         <h2>Contact</h2>
