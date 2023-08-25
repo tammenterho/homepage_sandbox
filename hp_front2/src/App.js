c2b4dcf8ce2ce9662c8b572c4b47afbe035f89a8
tammenterho <terho.mikko@gmail.com>
2023-08-25 10:46:03 +0300

    slick-carousel

--- hp_front2/src/App.js
+++ hp_front2/src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import React from "react";
+import SimpleSlider from "./SlickCarousel";
 
 function App() {
   return (
@@ -23,67 +25,8 @@ function App() {
           </p>
         </div>
       </div>
-      <div className="works">
-        <div>
-          <h3>Client Case 1: WhimsicalWares E-Commerce</h3>
-          <div className="clientcase case1">
-            <img
-              src={require("./whimsicalwares.png")}
-              alt="cheeky sorbet logo"
-            />
-            <p>
-              For WhimsicalWares, an online store selling unique handcrafted
-              goods, we concocted a social media strategy that combined quirky
-              product showcases with playful storytelling. By infusing humor and
-              relatability into their posts, we boosted engagement and
-              transformed their followers into avid customers, turning each
-              scroll into a delightful journey.
-            </p>
-          </div>
-        </div>
-        <div>
-          <h3>Client Case 2: MemeMasters Gaming</h3>
-          <div className="clientcase case2">
-            <img src={require("./mememasters.png")} alt="cheeky sorbet logo" />
-            <p>
-              MemeMasters Gaming wanted to level up their social media game. We
-              crafted a campaign that fused gaming humor with viral trends,
-              skyrocketing their follower count and building an enthusiastic
-              community. Our cheeky posts and interactive challenges turned
-              MemeMasters into more than just a game – it became an experience
-              worth sharing.
-            </p>
-          </div>
-        </div>
-        <div>
-          <h3>Client Case 3: FitFunnies Fitness</h3>
-          <div className="clientcase case3">
-            <img src={require("./mememasters.png")} alt="cheeky sorbet logo" />
-            <p>
-              FitFunnies Fitness sought a way to stand out in the saturated
-              fitness market. We devised a content strategy that fused
-              motivational workout tips with amusing gym anecdotes. Through
-              relatable content, witty captions, and even the occasional workout
-              blooper, we helped FitFunnies Fitness become the go-to destination
-              for fitness enthusiasts seeking a laugh.
-            </p>
-          </div>
-        </div>
-        <div>
-          <h3>Client Case 4: BuzzBites Café</h3>
-          <div className="clientcase case4">
-            <img src={require("./mememasters.png")} alt="cheeky sorbet logo" />
-            <p>
-              To give BuzzBites Café a distinctive online presence, we whipped
-              up a campaign that combined mouthwatering food visuals with
-              humor-filled captions. From playful coffee puns to witty
-              descriptions of their delectable treats, our strategy transformed
-              BuzzBites into an Instagram sensation that not only satisfied
-              cravings but also tickled taste buds.
-            </p>
-          </div>
-        </div>
-      </div>
+      <div className="works"></div>
+      <SimpleSlider />
     </div>
   );
 }
---
+++ hp_front2/src/SlickCarousel.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+    };
+    return (
+      <div>
+        <h2> Works</h2>
+        <Slider {...settings}>
+          <div className="works">
+            <h3>Client Case 1: WhimsicalWares E-Commerce</h3>
+            <div className="clientcase case1">
+              <img
+                src={require("./whimsicalwares.png")}
+                alt="cheeky sorbet logo"
+              />
+              <p>
+                For WhimsicalWares, an online store selling unique handcrafted
+                goods, we concocted a social media strategy that combined quirky
+                product showcases with playful storytelling. By infusing humor
+                and relatability into their posts, we boosted engagement and
+                transformed their followers into avid customers, turning each
+                scroll into a delightful journey.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h3>Client Case 2: MemeMasters Gaming</h3>
+              <div className="clientcase case2">
+                <img
+                  src={require("./mememasters.png")}
+                  alt="cheeky sorbet logo"
+                />
+                <p>
+                  MemeMasters Gaming wanted to level up their social media game.
+                  We crafted a campaign that fused gaming humor with viral
+                  trends, skyrocketing their follower count and building an
+                  enthusiastic community. Our cheeky posts and interactive
+                  challenges turned MemeMasters into more than just a game – it
+                  became an experience worth sharing.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h3>Client Case 3: FitFunnies Fitness</h3>
+            <div className="clientcase case3">
+              <img
+                src={require("./mememasters.png")}
+                alt="cheeky sorbet logo"
+              />
+              <p>
+                FitFunnies Fitness sought a way to stand out in the saturated
+                fitness market. We devised a content strategy that fused
+                motivational workout tips with amusing gym anecdotes. Through
+                relatable content, witty captions, and even the occasional
+                workout blooper, we helped FitFunnies Fitness become the go-to
+                destination for fitness enthusiasts seeking a laugh.
+              </p>
+            </div>
+          </div>
+          <div>
+            <h3>Client Case 4: BuzzBites Café</h3>
+            <div className="clientcase case4">
+              <img
+                src={require("./mememasters.png")}
+                alt="cheeky sorbet logo"
+              />
+              <p>
+                To give BuzzBites Café a distinctive online presence, we whipped
+                up a campaign that combined mouthwatering food visuals with
+                humor-filled captions. From playful coffee puns to witty
+                descriptions of their delectable treats, our strategy
+                transformed BuzzBites into an Instagram sensation that not only
+                satisfied cravings but also tickled taste buds.
+              </p>
+            </div>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
