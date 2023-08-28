16b886feb8a9f4d924b0b55f3640f1051d124e5e
tammenterho <terho.mikko@gmail.com>
2023-08-28 15:53:26 +0300

    fonts

--- hp_front2/src/App.js
+++ hp_front2/src/App.js
@@ -1,16 +1,18 @@
-import "./App.css";
 import React from "react";
 import SimpleSlider from "./SlickCarousel";
+import { Helmet } from "react-helmet";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <div className="App">
+        <Helmet>
+          <title>My App</title>
+          <link rel="stylesheet" href="https://use.typekit.net/sdb5sez.css" />
+          <meta name="description" content="My app description" />
+        </Helmet>
         <div className="header">
-          <img
-            src={require("./hp_front2_header_img.jpg")}
-            alt="cheeky sorbet logo"
-          />
           <h1 className="otsikko">Cheeky Social Sorbet</h1>
           <div className="nav">
             <p>works</p>
@@ -25,67 +27,68 @@ function App() {
           </p>
         </div>
       </div>
-      <div>
+      <div className="materials">
         <h2 className="works-header"> Works</h2>
-      </div>
-      <div className="carousel">
-        <SimpleSlider />
-      </div>
-      <div className="aboutus">
-        <h2>About us</h2>
-      </div>
-      <div className="aboutus-cont">
-        <div className="iltacont">
-          <div className="ilta">
-            <h3>Ilta Hämähäkkänen</h3>
-            <h4>Creative Visionary</h4>
-            <p>
-              Ilta is the visionary guiding the creative direction at Cheeky
-              Social Sorbet. Her unique perspective and innovative solutions
-              make her an inspiration to the entire team.
-            </p>
-          </div>
-          <img
-            className="iltanaama"
-            src={require("./ilta_naama.png")}
-            alt="cheeky sorbet logo"
-          />
+        <div className="carousel">
+          <SimpleSlider />
         </div>
-        <div className="mikaelcont">
-          <img
-            className="mikaelnaama"
-            src={require("./mikael_naama.png")}
-            alt="cheeky sorbet logo"
-          />
-          <div className="mikael">
-            <h3>Mikael Nautavaara</h3>
-            <h4>Visual Artisan</h4>
-            <p>
-              Mikael crafts visual masterpieces that elevate the brands of
-              Cheeky Social Sorbet's clients to new heights. His ability to
-              combine colors, shapes, and narratives is unparalleled.
-            </p>
+        <div className="aboutus">
+          <h2>About us</h2>
+        </div>
+        <div className="aboutus-cont">
+          <div className="iltacont">
+            <div className="ilta">
+              <h3>Ilta Hämähäkkänen</h3>
+              <h4>Creative Visionary</h4>
+              <p>
+                Ilta is the visionary guiding the creative direction at Cheeky
+                Social Sorbet. Her unique perspective and innovative solutions
+                make her an inspiration to the entire team.
+              </p>
+            </div>
+            <img
+              className="iltanaama"
+              src={require("./ilta_naama.png")}
+              alt="cheeky sorbet logo"
+            />
+          </div>
+          <div className="mikaelcont">
+            <img
+              className="mikaelnaama"
+              src={require("./mikael_naama.png")}
+              alt="cheeky sorbet logo"
+            />
+            <div className="mikael">
+              <h3>Mikael Nautavaara</h3>
+              <h4>Visual Artisan</h4>
+              <p>
+                Mikael crafts visual masterpieces that elevate the brands of
+                Cheeky Social Sorbet's clients to new heights. His ability to
+                combine colors, shapes, and narratives is unparalleled.
+              </p>
+            </div>
+          </div>
+          <div className="sylvicont">
+            <div className="sylvi">
+              <h3>Sylvi-Petra Lehti-Kokkeli</h3>
+              <h4>Social Alchemist</h4>
+              <p>
+                Sylvi is a social media magician conjuring enchanting campaigns
+                at Cheeky Social Sorbet. Her deep understanding of algorithms
+                and knack for predicting trends make her a true social media
+                expert.
+              </p>
+            </div>
+            <img
+              className="sylvinaama"
+              src={require("./sylvi_naama.png")}
+              alt="cheeky sorbet logo"
+            />
           </div>
         </div>
-        <div className="sylvicont">
-          <div className="sylvi">
-            <h3>Sylvi-Petra Lehti-Kokkeli</h3>
-            <h4>Social Alchemist</h4>
-            <p>
-              Sylvi is a social media magician conjuring enchanting campaigns at
-              Cheeky Social Sorbet. Her deep understanding of algorithms and
-              knack for predicting trends make her a true social media expert.
-            </p>
-          </div>
-          <img
-            className="sylvinaama"
-            src={require("./sylvi_naama.png")}
-            alt="cheeky sorbet logo"
-          />
+        <div className="contact">
+          <h2>Contact</h2>
         </div>
-      </div>
-      <div className="contact">
-        <h2>Contact</h2>
       </div>
     </div>
   );
