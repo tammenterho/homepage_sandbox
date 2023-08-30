09df6d38f2c7c7ef2e4d03cc6b201f5fbdb96202
tammenterho <terho.mikko@gmail.com>
2023-08-30 13:06:06 +0300

    more clients

--- hp_front2/src/App.js
+++ hp_front2/src/App.js
@@ -19,11 +19,45 @@ function App() {
       <div className="materials">
         <SimpleSlider />
         <div className="logocont">
-          here logos
-          <img
-            src={require("./images/Teknosolutions-logo.png")}
-            alt="cheeky sorbet logo"
-          />
+          <h2>More our clients</h2>
+          <div className="logos">
+            <img
+              src={require("./images/Teknosolutions-logo.png")}
+              alt="teknosolutions"
+            />
+            <img src={require("./images/company1.png")} alt="teknosolutions" />
+            <img src={require("./images/company2.png")} alt="teknosolutions" />
+            <img src={require("./images/company3.png")} alt="teknosolutions" />
+            <img src={require("./images/company4.png")} alt="teknosolutions" />
+            <img
+              src={require("./images/Teknosolutions-logo.png")}
+              alt="teknosolutions"
+            />
+            <img
+              src={require("./images/Teknosolutions-logo.png")}
+              alt="teknosolutions"
+            />
+            <img
+              src={require("./images/Teknosolutions-logo.png")}
+              alt="teknosolutions"
+            />
+            <img
+              src={require("./images/Teknosolutions-logo.png")}
+              alt="teknosolutions"
+            />
+            <img
+              src={require("./images/Teknosolutions-logo.png")}
+              alt="teknosolutions"
+            />
+            <img
+              src={require("./images/Teknosolutions-logo.png")}
+              alt="teknosolutions"
+            />
+            <img
+              src={require("./images/Teknosolutions-logo.png")}
+              alt="teknosolutions"
+            />
+          </div>
         </div>
         <AboutUs />
         <div className="contact">
