43f87abcfb975c897c12347c6fac20763bf1533a
tammenterho <terho.mikko@gmail.com>
2023-08-30 14:14:36 +0300

    contact form

--- hp_front2/src/App.js
+++ hp_front2/src/App.js
@@ -6,6 +6,7 @@ import AboutUs from "./aboutUs";
 import Header from "./header";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
+import { Button, Grid } from "@mui/material";
 
 function App() {
   return (
@@ -65,37 +66,82 @@ function App() {
         <div className="contact">
           <h2>Contact</h2>
           <div className="contact-input">
-            <div>
-              <TextField
-                className="textfield"
-                required
-                id="outlined-text"
-                label="First name"
-                type="text"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-              <TextField
-                required
-                id="outlined-text"
-                label="Last name"
-                type="text"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-              <TextField
-                required
-                id="outlined-number"
-                label="Number"
-                type="number"
-                helperText="Some important text"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </div>
+            <Grid container direction={"column"} spacing={5}>
+              <Grid item>
+                <TextField
+                  className="textfield"
+                  required
+                  id="outlined-text"
+                  label="First name"
+                  type="text"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+                <TextField
+                  required
+                  id="outlined-text"
+                  label="Last name"
+                  type="text"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  required
+                  id="outlined-number"
+                  label="Number"
+                  type="number"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  required
+                  sx={{
+                    width: 500,
+                  }}
+                  id="outlined-text"
+                  label="E-mail"
+                  type="text"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  required
+                  sx={{
+                    width: 500,
+                  }}
+                  id="outlined-multiline-flexible"
+                  label="Message"
+                  multiline
+                  maxRows={4}
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              variant="contained"
+              sx={{
+                width: 500,
+              }}
+            >
+              SEND
+            </Button>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
           </div>
         </div>
       </div>
