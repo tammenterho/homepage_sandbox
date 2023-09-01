10b7096a3d54b211e8b8a0e3a36e976c58f10c0d
tammenterho <terho.mikko@gmail.com>
2023-09-01 12:55:52 +0300

    styles

--- hp_front2/src/components/moreClients.jsx
+++ hp_front2/src/components/moreClients.jsx
@@ -5,10 +5,6 @@ export default function MoreClients() {
     <div className="logocont">
       <h2 className="clientstitle">More our clients</h2>
       <div className="logos">
-        <img
-          src={require("./../images/Teknosolutions-logo.png")}
-          alt="teknosolutions"
-        />
         <img src={require("./../images/company1.png")} alt="teknosolutions" />
         <img src={require("./../images/company2.png")} alt="teknosolutions" />
         <img src={require("./../images/company3.png")} alt="teknosolutions" />
