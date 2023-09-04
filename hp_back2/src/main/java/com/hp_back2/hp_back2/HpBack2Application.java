b0c53876b2f05c04181bd9ac480f9f31cf5be448
tammenterho <terho.mikko@gmail.com>
2023-09-04 13:55:03 +0300

    postgres connection for server

--- hp_back2/src/main/java/com/hp_back2/hp_back2/HpBack2Application.java
+++ hp_back2/src/main/java/com/hp_back2/hp_back2/HpBack2Application.java
@@ -3,10 +3,23 @@ package com.hp_back2.hp_back2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class HpBack2Application {
 
 	public static void main(String[] args) {
+		
+		// Lataa ympäristömuuttujat .env-tiedostosta
+        Dotenv dotenv = Dotenv.load();
+        System.out.println("Moi");
+
+        // Hae salasana ympäristömuuttujana
+        String password = dotenv.get("SPRING_DATASOURCE_PASSWORD");
+
+        // Voit käyttää salasanaa application.properties-tiedostossa
+        System.setProperty("spring.datasource.password", password);
+		
 		SpringApplication.run(HpBack2Application.class, args);
 	}
 
