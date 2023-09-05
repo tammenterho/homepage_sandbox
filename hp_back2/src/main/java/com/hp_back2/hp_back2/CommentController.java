8f864ed09e9b1c68b17848249c1ab77bc5b0380c
tammenterho <terho.mikko@gmail.com>
2023-09-05 19:23:35 +0300

    delete added

--- hp_back2/src/main/java/com/hp_back2/hp_back2/CommentController.java
+++ hp_back2/src/main/java/com/hp_back2/hp_back2/CommentController.java
@@ -3,7 +3,9 @@ package com.hp_back2.hp_back2;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,11 @@ public class CommentController {
 	 @PostMapping("/comments")
 	 Comment newComment(@RequestBody Comment newComment) {
 		 return commentRepo.save(newComment);
+	 }
+	 
+	 @DeleteMapping("/comments/{id}")
+	 void deleteComment(@PathVariable Integer id) {
+		 commentRepo.deleteById(id);
 	 }
 
 }
