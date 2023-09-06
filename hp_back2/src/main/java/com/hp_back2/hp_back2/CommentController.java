cfeb4633e364cd41ca12f424b439085da860f9d1
tammenterho <terho.mikko@gmail.com>
2023-09-06 08:34:51 +0300

    responseEntity for delete

--- hp_back2/src/main/java/com/hp_back2/hp_back2/CommentController.java
+++ hp_back2/src/main/java/com/hp_back2/hp_back2/CommentController.java
@@ -3,6 +3,8 @@ package com.hp_back2.hp_back2;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,8 +32,13 @@ public class CommentController {
 	 }
 	 
 	 @DeleteMapping("/comments/{id}")
-	 void deleteComment(@PathVariable Integer id) {
-		 commentRepo.deleteById(id);
+	 public ResponseEntity<String> deleteComment(@PathVariable Integer id) {
+	     try {
+	         commentRepo.deleteById(id);
+	         return new ResponseEntity<>("Comment deleted successfully", HttpStatus.OK);
+	     } catch (Exception e) {
+	         return new ResponseEntity<>("Failed to delete comment", HttpStatus.INTERNAL_SERVER_ERROR);
+	     }
 	 }
 
 }
