48488536be5e002a54cf3fe81804ee273e4c5c5d
tammenterho <terho.mikko@gmail.com>
2023-09-15 14:50:00 +0300

    putmapping

--- hp_back2/src/main/java/com/hp_back2/hp_back2/CommentController.java
+++ hp_back2/src/main/java/com/hp_back2/hp_back2/CommentController.java
@@ -1,6 +1,7 @@
 package com.hp_back2.hp_back2;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,5 +45,17 @@ public class CommentController {
 	         return new ResponseEntity<>("Failed to delete comment", HttpStatus.INTERNAL_SERVER_ERROR);
 	     }
 	 }
-
+	 
+	 @CrossOrigin(origins = "http://localhost:3000")
+	 @PutMapping("/comments/{id}")
+	 Optional<Comment> replaceComment(@RequestBody Comment newComment, @PathVariable Integer id) {
+		 
+		 return commentRepo.findById(id)
+				 .map(comment -> {
+					 comment.setName(newComment.getName());
+					 comment.setComment(newComment.getComment());
+					 return commentRepo.save(comment);
+					 
+				 });		 
+	 }
 }
